692bafb9b6cbe2323351f0b452ecd39229bb55c9
temirlan2301 <temirlanosmonbekov06@gmail.com>
2022-06-13 18:08:30 +0600

    dz3

---
+++ dz3/main.js
@@ -0,0 +1,19 @@
+// //1
+function numbers(num1,num2) {
+    const M = Math.min(number.number1, number.number2)
+    console.log(M)
+}
+ const number = {
+    number1: prompt("num1?"),
+    number2: prompt("num2?")
+ };
+
+numbers(number.number1, number.number2);
+
+//2
+
+function countChar() {
+    const num = prompt('???')
+    console.log(num.length)
+}
+countChar()
---
+++ lesson3/main.js
@@ -0,0 +1,69 @@
+// function sayhello() {
+// console.log("hello")
+// }
+//
+//  sayhello()
+
+// function getfullname(user) {
+//   if (age < 18){
+//       alert("close")
+//   }else{
+//       alert(user.first_name + " " + user.last_name + " " + "open")
+//   }
+// }
+//
+// const user = {
+//     first_name: "john",
+//     last_name: "barbaro"
+// }
+//
+// const age = prompt("сколько лет?")
+//
+// getfullname(user)
+
+// const anonym = function (users) {
+//         console.log(user, users)
+// }
+//
+// anonym()
+
+// function numbers(number1, number2) {
+//         if (nummber1 > number2){
+//             console.log("первое больше")
+//         }else if (nummber2 > number1){
+//             console.log("второе больше")
+//         }else{
+//             console.log("они равны")
+//         }
+// }
+//
+// const number = {
+//     number1: prompt("число?"),
+//     number2: prompt("число?")
+// }
+//
+// numbers(number.number1, number.number2)\
+
+
+// function twoMass(arr1, arr2 ) {
+//         if (arr1.length > arr2.length){
+//             console.log("первый больше")
+//         }else if (arr2.length > arr1.length){
+//             console.log("второе больше")
+//         }else {
+//             console.log("равны")
+//         }
+// }
+//
+// const arr1  = [23, 232, 3245, 4]
+// const  arr2 = [234, 43, 3, 43, 53, 53, 34]
+//
+// twoMass(arr1, arr2)
+
+// const  input = document.getElementById("input")
+// const   button = document.getElementById('button')
+//
+// button.onclick = function () {
+//     console.log(input.value)
+//
+// }
